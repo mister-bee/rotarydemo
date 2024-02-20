a0e741b8a44ea69493e5d9fdd0f68aa9142e7a89
Adam Boettcher <thebeech@yahoo.com>
2024-02-19 21:13:55 -0800

    add image and css touchup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import rotaryLogo from './assets/rotary-gold.png'; // Adjust the path as necessary
+
 import './App.css'
 
 function App() {
@@ -8,12 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={rotaryLogo} className="logo" alt="Rotary logo" />
       </div>
       <h1>Rotary Club Bot</h1>
 
