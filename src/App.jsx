f110e2a9d5d97e5d0f9b75192de10affe6dc40ea
Adam Boettcher <thebeech@yahoo.com>
2024-02-21 12:34:58 -0800

    add chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 import rotaryLogo from './assets/rotary-gold.png'; // Adjust the path as necessary
 
+import { BubbleChat } from 'flowise-embed-react'
 import './App.css'
+
+const ChatBot = () => {
+  return (
+    <BubbleChat chatflowid="7dec31ce-0193-4b89-8659-95ddbd65d04f" apiHost="https://flowiserotary.onrender.com" />
+  );
+};
+
 
 function App() {
 
@@ -11,6 +19,7 @@ function App() {
         <img src={rotaryLogo} className="logo" alt="Rotary logo" />
       </div>
       <h1>Rotary Club Bot</h1>
+      <ChatBot />
 
     </>
   )
