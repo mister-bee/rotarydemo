6df0b42d3bdb427d364aeb26d73cd75b8c074550
Adam Boettcher <thebeech@yahoo.com>
2024-02-21 18:22:54 -0800

    add fullscreen versino of bot for screen sizes under 640px

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
-import { useState } from 'react'
-import rotaryLogo from './assets/rotary-gold.png'; // Adjust the path as necessary
-
-import { BubbleChat } from 'flowise-embed-react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import rotaryLogo from './assets/rotary-gold.png';
+import { BubbleChat, FullPageChat } from 'flowise-embed-react';
+import './App.css';
 
 const ChatBot = () => {
   return (
-    <BubbleChat chatflowid="7dec31ce-0193-4b89-8659-95ddbd65d04f" apiHost="https://flowiserotary.onrender.com" />
+    <BubbleChat
+      chatflowid="7dec31ce-0193-4b89-8659-95ddbd65d04f"
+      apiHost="https://flowiserotary.onrender.com" />
   );
 };
 
+const FullPageChatBot = () => {
+  return (
+    <FullPageChat
+      chatflowid="7dec31ce-0193-4b89-8659-95ddbd65d04f"
+      apiHost="https://flowiserotary.onrender.com"
+    />
+  );
+};
 
 function App() {
+  const [isNarrowScreen, setIsNarrowScreen] = useState(window.innerWidth < 640);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsNarrowScreen(window.innerWidth < 640);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <>
-      <div>
-        <img src={rotaryLogo} className="logo" alt="Rotary logo" />
-      </div>
+      <img src={rotaryLogo} className="logo" alt="Rotary logo" />
       <h1>Rotary Club Bot</h1>
-      <ChatBot />
-
+      {isNarrowScreen ? <FullPageChatBot /> : <ChatBot />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
